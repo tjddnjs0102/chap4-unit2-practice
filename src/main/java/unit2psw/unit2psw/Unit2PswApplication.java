4f13c15ba917cde964e5823d693bd4b7d03d467e
tjddnjs0102 <rjf654@gmail.com>
2023-08-08 03:20:24 +0900

    initial connect repository

---
+++ src/main/java/unit2psw/unit2psw/Unit2PswApplication.java
@@ -0,0 +1,13 @@
+package unit2psw.unit2psw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit2PswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit2PswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unit2psw/unit2psw/Unit2PswApplicationTests.java
@@ -0,0 +1,13 @@
+package unit2psw.unit2psw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit2PswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
